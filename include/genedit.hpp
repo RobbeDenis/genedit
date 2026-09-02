95a115fe5fe76845cd2b8f23b7b548d71e1910c5
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-09-02 20:55:53 +0200

    Tested interpreting dna with segment marker

--- include/genedit.hpp
+++ include/genedit.hpp
@@ -10,19 +10,45 @@
 
 using DNA = uint64_t;
 
-struct Interpreter {
-	std::vector<char> start_markers;
-	std::vector<char> stop_markers;
+struct SegmentMarker {
+	uint8_t start;
+	uint8_t stop;
 };
 
 struct Genome {
-	uint32_t data;
+	std::vector<uint64_t> data;
 };
 
 [[nodiscard]] DNA random_dna() {
 	static std::mt19937 gen(std::random_device{}());
 	std::uniform_int_distribution<uint64_t> dna_dist(0ull);
 	return DNA{ dna_dist(gen) };
+}
+
+[[nodiscard]] Genome interpret_dna(DNA dna, SegmentMarker marker) {
+	Genome result{ };
+	auto bytes = std::bit_cast<std::array<uint8_t, sizeof(DNA)>>(dna);
+	uint64_t segment{ 0 };
+	int shift{ 0 };
+	bool reading{ false };
+
+	for (size_t i{ 0 }; i < sizeof(dna); ++i) {
+		if (reading && bytes[i] == marker.stop) {
+			reading = false;
+			result.data.push_back(segment);
+			segment = 0;
+			shift = 0;
+		}
+		else if (bytes[i] == marker.start) {
+			reading = true;
+		}
+		else {
+			segment += static_cast<uint64_t>(bytes[i]) << shift * 8;
+			++shift;
+		}
+	}
+
+	return result;
 }
 
 [[nodiscard]] std::string dna_to_hex(DNA dna) {
--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@
 
 int main()
 {
+    using namespace ftxui;
+
     DNA dna_1{ random_dna() };
     DNA dna_2{ random_dna() };
     DNA dna_3{ random_dna() };
+
+    const SegmentMarker marker{ .start = 1ull, .stop = 2ull };
+
+    DNA test_dna{ marker.start };
+    test_dna += 240ull << 8;
+    test_dna += 108ull << 16;
+    test_dna += 23ull << 24;
+    test_dna += static_cast<uint64_t>(marker.stop) << 32;
+
+    Genome test_genome{ interpret_dna(test_dna, marker) };
+    Elements test_text{ };
+    for (size_t i = 0; i < test_genome.data.size(); ++i) {
+        test_text.push_back(text(dna_to_hex(test_genome.data[i])) | border | color(Color::Green));
+    }
     
-    using namespace ftxui;
     auto document =
         vbox({
+            text("Random DNA"),
             hbox({ text("DNA    " + dna_to_hex(dna_1)) | border }),
             hbox({ text("DNA    " + dna_to_hex(dna_2)) | border }),
-            hbox({ text("DNA    " + dna_to_hex(dna_3)) | border })
+            hbox({ text("DNA    " + dna_to_hex(dna_3)) | border }),
+            text("Test DNA"),
+            hbox({ text("DNA    " + dna_to_hex(test_dna)) | border, hbox(test_text) })
         });
 
     auto screen = Screen::Create(Dimension::Full());
-    Render(screen, document);
+    ftxui::Render(screen, document);
     screen.Print();
 
     return 0;
